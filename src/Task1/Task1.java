db5550dd66e1e8f32b2e22beb253e78cd57c56ff
argentoff1 <mparinos56@gmail.com>
2022-08-10 05:49:00 +0300

    Сделан шаблон репозитория

---
+++ src/Task1/Task1.java
@@ -0,0 +1,23 @@
+package Task1;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        int fareCost = scanner.nextInt();
+        int fareAmount = scanner.nextInt();
+        int extraMbCost = scanner.nextInt();
+        int trafficVolume = scanner.nextInt();
+        int sum = 0;
+
+        if(trafficVolume > fareAmount) {
+            sum = fareCost + extraMbCost * (trafficVolume - fareAmount);
+        }
+        else if(fareAmount >= trafficVolume)
+            sum = fareCost;
+        System.out.println(sum);
+    }
+}
